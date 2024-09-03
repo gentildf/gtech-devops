d3b3bdafc9ba8ba2a9feeaa8365b10b6e1f91756
gentildf <gentildf@gmail.com>
2024-09-02 22:46:54 -0300

    tt

--- ci-github-actions/tests/test_app.py
+++ ci-github-actions/tests/test_app.py
@@ -1,14 +1,13 @@
+from app import add  # importando modulo
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-
-from app import add  # Agora você pode importar o módulo corretamente
-
 import unittest
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 class TestApp(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(1, 2), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
